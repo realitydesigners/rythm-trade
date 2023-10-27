3b18983d7a7ce511013837e92d87e3ddb81dc3ec
RD <raymond.luke.spartz@gmail.com>
2023-10-26 23:16:44 -0700

    mock box

--- rythm/app/page.tsx
+++ rythm/app/page.tsx
@@ -4,11 +4,14 @@ import DataTable from "./components/DataTable";
 import Stream from "./components/Stream";
 import CandleChart from "./components/LineChart";
 import { fetchData } from './api/getData'; 
+import Box from './components/Box';
 
 const API_INTERVAL = 5000;  // 5 seconds
+const BOX_SIZE = 1;  // Define your box size here, assuming 10 for this example
 
 function App() {
   const [candleData, setCandleData] = useState([]);
+  const [currentPrice, setCurrentPrice] = useState(0);  // New state for current price
 
   useEffect(() => {
     const fetchCandleData = async () => {
@@ -17,12 +20,18 @@ function App() {
         const candles = data?.candles;
 
         setCandleData(candles || []);
+
+        // Update the current price based on the last available candle's close price
+        const latestPrice = candles?.[candles.length - 1]?.close?.out; 
+        if (latestPrice) {
+          setCurrentPrice(latestPrice);
+        }
       } catch (error) {
         console.error('Error fetching candle data:', error);
       }
     };
-    fetchCandleData();
 
+    fetchCandleData();
 
     const interval = setInterval(fetchCandleData, API_INTERVAL);
 
@@ -32,6 +41,8 @@ function App() {
   return (
     <div>
       <Stream />
+      {/* Pass the required props to the Box component */}
+      <Box currentPrice={currentPrice} boxSize={BOX_SIZE} candleData={candleData} />
       <CandleChart data={candleData} />
       <DataTable />
     </div>
