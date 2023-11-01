3cfbf45a3416fbf9bb38b23924f17ec1fcc64a3a
RD <raymond.luke.spartz@gmail.com>
2023-11-01 15:49:33 -0700

    Added Pairs

--- rythm/app/dashboard/page.tsx
+++ rythm/app/dashboard/page.tsx
@@ -1,11 +1,20 @@
 "use client";
-import React from 'react';
+import React from 'react'
+import Link  from 'next/link'
 
 const DashboardPage = () => {
+  const currencyPairs = ['gbpusd', 'eurusd', 'usdjpy', 'usdcad', 'usdchf', 'audusd'];
+
   return (
-    <div className='w-full pl-10 pr-10 pt-2 pb-2 bg-black rounded-lg shadow-lg text-sm space-y-1 font-mono text-gray-200'>
-      <h1 className='text-gray-400 font-bold border-b border-gray-600 pb-2'>Dashboard</h1> 
-      <a href="/dashboard/pairs/gbpusd" className='text-gray-400 font-bold'>GBP/USD</a>
+    <div className='w-full p-4 bg-black rounded-lg shadow-lg text-sm space-y-1 font-mono text-gray-200'>
+      <h1 className='text-gray-400 font-bold border-b border-gray-600 tracking-widest uppercase  pb-2'>Dashboard</h1> 
+      {currencyPairs.map(pair => (
+        <div key={pair}>
+          <Link href={`/dashboard/pairs/${pair}`} className='text-gray-400 text-lg tracking-wide font-bold hover:text-white'>
+            {pair.toUpperCase()}
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
