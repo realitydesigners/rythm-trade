245d64c73e0f7a4acc9c1b2c3105f81324032eb7
Mitch <jamlets@protonmail.com>
2023-11-14 16:47:45 -0500

    Box Update

--- rythm/app/components/BoxModel/index.tsx
+++ rythm/app/components/BoxModel/index.tsx
@@ -1,6 +1,6 @@
-"use client"
-// BoxModel/index.tsx
-import React, { useState, useEffect, useContext } from 'react';
+"use client";
+
+import React, { useState, useEffect, useContext, useCallback } from 'react';
 import styles from './styles.module.css';
 import { CandleData, Box } from '../../../types';
 import { findCurrentPrice, findHighest, findLowest } from '../../api/priceAnalysis';
@@ -9,52 +9,87 @@ import { symbolsToDigits } from '../../utils/constants';
 interface BoxModelProps {
     pair: string;
 } 
+
 const BoxModel: React.FC<BoxModelProps> = ({ pair }) => {
     const api = useContext(OandaApiContext);
-    
+
     const [currentClosePrice, setCurrentClosePrice] = useState<number | null>(null);
-    const [currentDirection, setCurrentDirection] = useState<string | null>(null);
     const [boxArray, setBoxArray] = useState<Box[]>([]);
     const [initializationComplete, setInitializationComplete] = useState<boolean>(false);
-    const [lowestIndex, setLowestIndex] = useState<number | null>(null);
-    const [highestIndex, setHighestIndex] = useState<number | null>(null);
 
-    const numBoxes = 5;
-    const boxSizePoints = 100.0;
+    const boxSizePoints = 10.0;
+    const numBoxes = 10;
+    
+    // Dynamically determine point value and digits based on the currency pair. 
+    const { point: pointValue, digits } = symbolsToDigits[pair] || { point: 0.0001, digits: 5 };
 
-    const BoxMovedUp = new Array(numBoxes).fill(false);
-    const BoxMovedDn = new Array(numBoxes).fill(false);
-    const RngHigh = new Array(numBoxes).fill(null);
-    const RngLow = new Array(numBoxes).fill(null);
-    const RngSize = new Array(numBoxes).fill(null);
+    // Callback function to calculate the boxes based on the current price and Oanda data. 
+    const calculateBoxes = useCallback((C: number, oandaData: CandleData[]) => {
+        let newBoxArray: Box[] = [];
+        let RngHigh = C;
+        let RngLow = C - boxSizePoints * pointValue;
+        let BoxMovedUp = false;
+        let BoxMovedDn = false;
+        let RngSize = RngHigh - RngLow;
 
-    let oandaData: CandleData[] = [];
-    let Point = .10; 
+        // Initialization logic to set the initial range of the boxes. 
+        if (!initializationComplete) { 
+            const highestHigh = findHighest(oandaData, 0, oandaData.length - 1) || C;
+            const lowestLow = findLowest(oandaData, 0, oandaData.length - 1) || C;
+            const RNG = highestHigh - lowestLow;
+    
+            if (RNG >= boxSizePoints * pointValue) {
+                RngHigh = highestHigh;
+                RngLow = highestHigh - boxSizePoints * pointValue;
+                BoxMovedUp = true;
+                BoxMovedDn = false;
+            } else {
+                RngLow = lowestLow;
+                RngHigh = lowestLow + boxSizePoints * pointValue;
+                BoxMovedUp = false;
+                BoxMovedDn = true;
+            }
+            RngSize = RngHigh - RngLow;
+            setInitializationComplete(true);
 
+        }
+
+        // Update the range and direction of the box based on the current price. 
+        if (C > RngHigh) {
+            RngHigh = C;
+            RngLow = RngHigh - boxSizePoints * pointValue;
+            BoxMovedUp = true;
+            BoxMovedDn = false;
+        } else if (C < RngLow) {
+            RngLow = C;
+            RngHigh = RngLow + boxSizePoints * pointValue;
+            BoxMovedUp = false;
+            BoxMovedDn = true;
+        }
+    
+        newBoxArray.push({
+            high: RngHigh,
+            low: RngLow,
+            boxMovedUp: BoxMovedUp,
+            boxMovedDn: BoxMovedDn,
+            rngSize: RngSize
+        });
+    
+        setBoxArray(newBoxArray);
+    }, [initializationComplete, pointValue]);
+    
+
+    // Effect to fetch data from the Oanda API and initialize box calculations. 
     useEffect(() => {
         const initializeData = async () => {
             try {
-                oandaData = await api?.fetchCandles(pair, 300, 'M1');
-
+                const oandaData = await api?.fetchCandles(pair, 300, 'M1');
                 if (oandaData && oandaData.length > 0) {
                     const currentPrice = findCurrentPrice(oandaData);
                     if (currentPrice !== undefined) {
                         setCurrentClosePrice(currentPrice);
+                        calculateBoxes(currentPrice, oandaData);
                     }
-
-                    const highestHighIdx = findHighest(oandaData, 0, oandaData.length - 1) || null;
-                    const lowestLowIdx = findLowest(oandaData, 0, oandaData.length - 1) || null;
-
-                    setHighestIndex(highestHighIdx);
-                    setLowestIndex(lowestLowIdx);
-
-                    const previousPrice = parseFloat(oandaData[1].mid.c);
-                    if (currentPrice !== undefined) {
-                        setCurrentDirection(currentPrice > previousPrice ? 'UP' : 'DOWN');
-                        calculateBoxes(currentPrice, boxSizePoints, oandaData, false);
-                    }
-
-                    setInitializationComplete(true);
                 } else {
                     console.log("No valid data received.");
                 }
@@ -64,115 +99,22 @@ const BoxModel: React.FC<BoxModelProps> = ({ pair }) => {
         };
 
         initializeData();
-    }, []);
+    }, [api, pair, calculateBoxes]);
 
-    if (oandaData && oandaData.length > 0) {
-        const lastCandle = oandaData[oandaData.length - 1];
-        const C = parseFloat(lastCandle.mid.c);
-        // Now you can use C safely
-    } 
-    const getPairDetails = (pair: string) => {
-        const defaultDetails = { point: 0.0001, digits: 4 };
-        return symbolsToDigits[pair] || defaultDetails;
-    };
-
-    const calculateBoxes = (C: number, boxSizePoints: number, oandaData: CandleData[], quickInit: boolean) => {
-        const { point, digits } = getPairDetails(pair);
-
-        const theBoxSize = boxSizePoints * point;
-
-        if (typeof C === 'undefined' || isNaN(theBoxSize)) {
-            return;
-        }
-        console.log(oandaData)
-    
-        console.log("C:", C);
-        console.log("boxSizePoints:", boxSizePoints);
-    
-        if (quickInit) {
-            let minHH = Number.MAX_VALUE;
-            let maxLL = Number.MIN_VALUE;
-            for (let i = 0; i < oandaData.length; i++) {
-                const price = parseFloat(oandaData[i].mid.c);
-                if (price > maxLL) {
-                    maxLL = price;
-                }
-                if (price < minHH) {
-                    minHH = price;
-                }
-
-                if (maxLL - minHH >= boxSizePoints * Point) {
-                    break;
-                }
-            }
-
-            for (let b = 0; b < numBoxes; b++) {
-                RngHigh[b] = maxLL;
-                RngLow[b] = maxLL - boxSizePoints * Point;
-            }
-
-            console.log("Quick Init - RngHigh:", RngHigh);
-            console.log("Quick Init - RngLow:", RngLow);
-        }
-    
-        const symbol = pair
-
-        if (symbol in symbolsToDigits) {
-            const { point, digits } = symbolsToDigits[symbol];
-
-            const theBoxSize = boxSizePoints * Point;
-            
-
-            if (!isNaN(theBoxSize)) {
-                console.log("theBoxSize:", theBoxSize);
-
-                for (let b = 0; b < numBoxes; b++) {
-                    if (RngSize[b] < theBoxSize) {
-                        if (C > RngHigh[b]) {
-                            RngHigh[b] = C;
-                        } else if (C < RngLow[b]) {
-                            RngLow[b] = C;
-                        }
-                        continue;
-                    }
-
-                    if (C > RngHigh[b]) {
-                        RngHigh[b] = C;
-                        RngLow[b] = RngHigh[b] - theBoxSize;
-                        BoxMovedUp[b] = true;
-                        BoxMovedDn[b] = false;
-                    } else if (C < RngLow[b]) {
-                        RngLow[b] = C;
-                        RngHigh[b] = RngLow[b] + theBoxSize;
-                        BoxMovedUp[b] = false;
-                        BoxMovedDn[b] = true;
-                    }
-
-                    console.log(`Box ${b} - RngHigh: ${RngHigh}, RngLow: ${RngLow}`);
-                }
-            }
-        } else {
-            console.log(`Symbol ${symbol} not found in symbolsToDigits mapping.`);
-        }
-    };
-
-return (
-    <div className={styles.container}>
-        <div><span className={styles.title}>Current Close Price:</span> {currentClosePrice}</div>
-        <div><span className={styles.title}>Lowest Index:</span> {lowestIndex}</div>
-        <div><span className={styles.title}>Highest Index:</span> {highestIndex}</div>
-        <div><span className={styles.title}>Current Price vs Prev Price:</span> {currentDirection}</div>
-        <div><span className={styles.title}>Box Direction:</span>
-            <ul>
-                {boxArray.map((box, index) => (
-                    <li key={index}>
-                        Box {index + 1}: {box.boxMovedUp ? "UP" : box.boxMovedDn ? "DOWN" : "STABLE"}
-                    </li>
-                ))}
-            </ul>
+    return (
+        <div className={styles.container}>
+            <div><span className={styles.title}>Current Close Price:</span> {currentClosePrice}</div>
+            <div><span className={styles.title}>Box Direction:</span>
+                <ul>
+                    {boxArray.map((box, index) => (
+                        <li key={index}>
+                            Box {index + 1}: {box.boxMovedUp ? "UP" : box.boxMovedDn ? "DOWN" : "STABLE"}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
-    </div>
-);
-}
+    );
+};
 
 export default BoxModel;
