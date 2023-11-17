7e3a394ab653270a23fd409076d48f7cdfb4f746
RD <raymond.luke.spartz@gmail.com>
2023-11-17 15:34:02 -0800

    remove

--- rythm/index.ts
+++
@@ -1,6 +0,0 @@
-export const ACCOUNT_ID = "101-001-18616698-004";
-export const OANDA_TOKEN =
-  "b942e647b2bcaa718f1aca34fd56c794-32966a773e2bbd78295b47fd952f71fe";
-export const OANDA_BASE_URL = "https://api-fxpractice.oanda.com/v3";
-export const OANDA_STREAM_URL = "https://stream-fxpractice.oanda.com/v3";
-export const INSTRUMENT = "GBP_USD";
