7cc1f248c951ae4e9e9d1906d70c438ec913649e
Mitch <jamlets@protonmail.com>
2023-11-29 11:50:20 -0500

    prisma

--- ryver/src/services/DatabaseService.ts
+++ ryver/src/services/DatabaseService.ts
@@ -1,29 +1,16 @@
-import mysql from 'mysql2/promise';
+import { PrismaClient } from '@prisma/client';
 
 export class DatabaseService {
-  private connection: mysql.Connection | undefined;
+  private prisma: PrismaClient;
 
   constructor() {
-    this.init();
-  }
-
-  private async init() {
-    this.connection = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      ssl: {
-        rejectUnauthorized: true,
-      },
-    });
+    this.prisma = new PrismaClient();
   }
 
   public async getUser(userId: string) {
-    if (!this.connection) {
-      throw new Error('Database connection is not initialized');
-    }
-    const [rows] = await this.connection.execute('SELECT * FROM users WHERE id = ?', [userId]);
-    return rows;
+    const user = await this.prisma.user.findUnique({
+      where: { id: Number(userId) },
+    });
+    return user;
   }
 }
