7c12d0cca235fc5cf93002a71d4dfdfd84695f38
RD <raymond.luke.spartz@gmail.com>
2023-11-29 10:31:07 -0800

    fix style on slug page

--- rythm/app/dashboard/pairs/[pair]/page.tsx
+++ rythm/app/dashboard/pairs/[pair]/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import React, { useState, useEffect, useContext } from 'react';
 import { useParams } from 'next/navigation';
 import DataTable from '@/app/components/DataTable';
@@ -13,75 +13,71 @@ import ResoModel from '@/app/components/ResoModel';
 import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from '@/components/ui/select';
 import { BOX_SIZES } from '@/app/utils/constants';
 
-
-
 const PairPage = () => {
-  const params = useParams();
-  let pair = Array.isArray(params.pair) ? params.pair[0] : params.pair || ''; 
-  const [streamData, setStreamData] = useState<{ [pair: string]: any }>({}); 
-  const [positionData, setPositionData] = useState(null);
-  // State for selected box array type
-  const [selectedBoxArrayType, setSelectedBoxArrayType] = useState<string>('d');
+   const params = useParams();
+   let pair = Array.isArray(params.pair) ? params.pair[0] : params.pair || '';
+   const [streamData, setStreamData] = useState<{ [pair: string]: any }>({});
+   const [positionData, setPositionData] = useState(null);
+   // State for selected box array type
+   const [selectedBoxArrayType, setSelectedBoxArrayType] = useState<string>('d');
 
-  // Function to handle change in selected box array type
-  const handleBoxArrayTypeChange = (newType: string) => {
-    setSelectedBoxArrayType(newType);
-  };
-  useEffect(() => {
-    const handleStreamData = (data: any, pair: string) => {   
-      if (data.type !== "HEARTBEAT") {
-        setStreamData((prevData) => ({
-          ...prevData,
-          [pair]: data,
-        }));
-      }
-    };
-  
-    api.subscribeToPairs([pair], handleStreamData);
+   // Function to handle change in selected box array type
+   const handleBoxArrayTypeChange = (newType: string) => {
+      setSelectedBoxArrayType(newType);
+   };
+   useEffect(() => {
+      const handleStreamData = (data: any, pair: string) => {
+         if (data.type !== 'HEARTBEAT') {
+            setStreamData(prevData => ({
+               ...prevData,
+               [pair]: data,
+            }));
+         }
+      };
 
-    const fetchPosition = async () => {
-      const position = await api.getPairPositionSummary(pair);
-      setPositionData(position);
-    };
+      api.subscribeToPairs([pair], handleStreamData);
 
-    fetchPosition();
-    const intervalId = setInterval(fetchPosition, 1000);
+      const fetchPosition = async () => {
+         const position = await api.getPairPositionSummary(pair);
+         setPositionData(position);
+      };
 
-    return () => {
-      api.unsubscribeFromPairs([pair]);
-      clearInterval(intervalId);
-    };
-  }, [pair]);
-  
-  return (
-    <OandaApiContext.Provider value={api}>
+      fetchPosition();
+      const intervalId = setInterval(fetchPosition, 1000);
 
-      <div className={styles.container}>
-        <a href="/" className={styles.backLink}> --BACK</a>
-        <h1 className={styles.title}>{pair}</h1>
-        <Stream pair={pair} data={streamData[pair]} />
+      return () => {
+         api.unsubscribeFromPairs([pair]);
+         clearInterval(intervalId);
+      };
+   }, [pair]);
 
-        <ResoModel pair={pair} streamData={streamData[pair]} selectedBoxArrayType={selectedBoxArrayType} />
-        <Select value={selectedBoxArrayType} onValueChange={handleBoxArrayTypeChange}>
-          <SelectTrigger>
-            <SelectValue>{selectedBoxArrayType}</SelectValue>
-          </SelectTrigger>
-          <SelectContent>
-            {Object.keys(BOX_SIZES).map(arrayKey => (
-              <SelectItem key={arrayKey} value={arrayKey}>
-                {arrayKey}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-
-        <ElixrModel pair={pair} streamData={streamData[pair]} />
-
-        {positionData && <MasterPosition positionData={[positionData]} />}
-        {!positionData && <p>No position data available for {pair}</p>}
-      </div>
-    </OandaApiContext.Provider>
-  );
+   return (
+      <OandaApiContext.Provider value={api}>
+         <div className={`{styles.container} pt-20 `}>
+            <div className="w-full pl-6 pr-6">
+               <Stream pair={pair} data={streamData[pair]} />
+            </div>
+            <ResoModel pair={pair} streamData={streamData[pair]} selectedBoxArrayType={selectedBoxArrayType} />
+            <div className="w-full p-6">
+               <Select value={selectedBoxArrayType} onValueChange={handleBoxArrayTypeChange}>
+                  <SelectTrigger>
+                     <SelectValue>{selectedBoxArrayType}</SelectValue>
+                  </SelectTrigger>
+                  <SelectContent>
+                     {Object.keys(BOX_SIZES).map(arrayKey => (
+                        <SelectItem key={arrayKey} value={arrayKey}>
+                           {arrayKey}
+                        </SelectItem>
+                     ))}
+                  </SelectContent>
+               </Select>
+               <ElixrModel pair={pair} streamData={streamData[pair]} />
+            </div>
+            {positionData && <MasterPosition positionData={[positionData]} />}
+            {!positionData && <p>No position data available for {pair}</p>}
+         </div>
+      </OandaApiContext.Provider>
+   );
 };
 
 export default PairPage;
