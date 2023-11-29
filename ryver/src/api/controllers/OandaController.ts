b168689609e4d343392d40e547532f1b9fd41695
Mitch <jamlets@protonmail.com>
2023-11-29 10:38:45 -0500

    Begin Migrating

--- ryver/src/api/controllers/OandaController.ts
+++ ryver/src/api/controllers/OandaController.ts
@@ -1,25 +1,20 @@
-import { OandaApi } from "../../services/OandaApi";
+import { OandaApi } from '../../services/OandaApi';
 
 export class OandaController {
   private oandaApi: OandaApi;
 
   constructor() {
-    this.oandaApi = new OandaApi(
-      process.env.OANDA_TOKEN as string,
-      process.env.OANDA_BASE_URL as string,
-      process.env.ACCOUNT_ID as string,
-      process.env.OANDA_STREAM_URL as string
-    );
+    this.oandaApi = new OandaApi(process.env.OANDA_TOKEN as string, process.env.OANDA_BASE_URL as string, process.env.ACCOUNT_ID as string, process.env.OANDA_STREAM_URL as string);
   }
 
   public async getAccountSummary() {
-    console.log("Received request for getAccountSummary");
+    console.log('Received request for getAccountSummary');
     try {
       const summary = await this.oandaApi.getAccountSummary();
-      console.log("Account Summary:", summary);
+      console.log('Account Summary:', summary);
       return summary; // Directly return the summary
     } catch (error) {
-      console.error("Error in getAccountSummary:", error);
+      console.error('Error in getAccountSummary:', error);
       return { error: 'An error occurred while fetching account summary' }; // Return error response
     }
   }
