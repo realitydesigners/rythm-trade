cc3b47ae6c52cf5ff2a6697da9fccd6e7f2f748d
numbercutter <jamlets@protonmail.com>
2023-11-30 14:20:56 -0500

    Ws Example

--- rythm/app/dashboard/page.tsx
+++ rythm/app/dashboard/page.tsx
@@ -18,68 +18,101 @@ import { PositionData } from '@/types';
 const initialFavorites = ['GBP_USD', 'USD_JPY', 'AUD_USD', 'EUR_JPY', 'EUR_USD', 'USD_CAD', 'NZD_USD', 'GBP_JPY'];
 
 const DashboardPage = () => {
-   const [currencyPairs, setCurrencyPairs] = useState<string[]>([]);
-   const [favoritePairs, setFavoritePairs] = useState<string[]>(initialFavorites);
-   const [draggedItem, setDraggedItem] = useState<string | null>(null);
-   const [allPairs, setAllPairs] = useState<string[]>([]);
-   const [showProfile, setShowProfile] = useState(false);
-   const [numDisplayedFavorites, setNumDisplayedFavorites] = useState<number>(4);
-   const [streamData, setStreamData] = useState<{ [pair: string]: any }>({});
-   const [isLoading, setIsLoading] = useState(true); // Initialize as true to show loading by default
-   const [selectedBoxArrayTypes, setSelectedBoxArrayTypes] = useState(Object.fromEntries(initialFavorites.map(pair => [pair, 'd'])));
-   const [positionData, setPositionData] = useState<PositionData[]>([]);
+  const [currencyPairs, setCurrencyPairs] = useState<string[]>([]);
+  const [favoritePairs, setFavoritePairs] = useState<string[]>(initialFavorites);
+  const [draggedItem, setDraggedItem] = useState<string | null>(null);
+  const [allPairs, setAllPairs] = useState<string[]>([]);
+  const [showProfile, setShowProfile] = useState(false);
+  const [numDisplayedFavorites, setNumDisplayedFavorites] = useState<number>(4);
+  const [streamData, setStreamData] = useState<{ [pair: string]: any }>({});
+  const [isLoading, setIsLoading] = useState(true); // Initialize as true to show loading by default
+  const [selectedBoxArrayTypes, setSelectedBoxArrayTypes] = useState(Object.fromEntries(initialFavorites.map(pair => [pair, 'd'])));
+  const [positionData, setPositionData] = useState<PositionData[]>([]);
+  const [ws, setWs] = useState<WebSocket | null>(null);
 
-   const handleBoxArrayChange = (pair: string, selectedKey: string) => {
-      setSelectedBoxArrayTypes(prev => ({
-         ...prev,
-         [pair]: selectedKey,
-      }));
-   };
+  useEffect(() => {
+    if (ws && ws.readyState === WebSocket.OPEN) {
+      ws.close();
+    }
 
-   const toggleProfile = () => {
-      setShowProfile(prevShow => !prevShow);
-   };
-   useEffect(() => {
-      const fetchInstruments = async () => {
-         if (api) {
-            const instruments = await api.getAccountInstruments();
-            if (instruments) {
-               const allPairsFetched = instruments.map((inst: { name: string }) => inst.name);
-               const sortedPairs = allPairsFetched.sort((a: string, b: any) => a.localeCompare(b));
-               setAllPairs(sortedPairs);
-               setCurrencyPairs(sortedPairs);
-            }
-         }
-      };
-      fetchInstruments();
-   }, []);
+    const websocket = new WebSocket(process.env.NEXT_PUBLIC_WEBSOCKET_URL as string);
+    setWs(websocket);
 
-   useEffect(() => {
-      const handleStreamData = (data: any, pair: string) => {
-         if (data.type !== 'HEARTBEAT') {
-            setStreamData(prevData => ({
-               ...prevData,
-               [pair]: data,
-            }));
-         }
-      };
+    websocket.onopen = () => {
+      console.log('WebSocket Connected');
+    };
 
+    websocket.onmessage = event => {
+      const data = JSON.parse(event.data);
+      console.log('WebSocket Message:', data);
+    };
+
+    websocket.onerror = event => {
+      console.error('WebSocket Error:', event);
+    };
+
+    websocket.onclose = () => {
+      console.log('WebSocket Disconnected');
+    };
+
+    return () => {
+      if (websocket.readyState === WebSocket.OPEN) {
+        websocket.close();
+      }
+    };
+  }, []);
+
+  const handleBoxArrayChange = (pair: string, selectedKey: string) => {
+    setSelectedBoxArrayTypes(prev => ({
+      ...prev,
+      [pair]: selectedKey,
+    }));
+  };
+
+  const toggleProfile = () => {
+    setShowProfile(prevShow => !prevShow);
+  };
+  useEffect(() => {
+    const fetchInstruments = async () => {
+      if (api) {
+        const instruments = await api.getAccountInstruments();
+        if (instruments) {
+          const allPairsFetched = instruments.map((inst: { name: string }) => inst.name);
+          const sortedPairs = allPairsFetched.sort((a: string, b: any) => a.localeCompare(b));
+          setAllPairs(sortedPairs);
+          setCurrencyPairs(sortedPairs);
+        }
+      }
+    };
+    fetchInstruments();
+  }, []);
+
+  useEffect(() => {
+    const handleStreamData = (data: any, pair: string) => {
+      if (data.type !== 'HEARTBEAT') {
+        setStreamData(prevData => ({
+          ...prevData,
+          [pair]: data,
+        }));
+      }
+    };
+
+    api.unsubscribeFromPairs(favoritePairs);
+    api.subscribeToPairs(favoritePairs, handleStreamData);
+
+    return () => {
       api.unsubscribeFromPairs(favoritePairs);
-      api.subscribeToPairs(favoritePairs, handleStreamData);
+    };
+  }, [favoritePairs]);
+  // Fetch position data periodically
+  useEffect(() => {
+    const fetchPositionData = async () => {
+      const positions = await api.getAllPositions();
+      setPositionData(positions);
+    };
 
-      return () => {
-         api.unsubscribeFromPairs(favoritePairs);
-      };
-   }, [favoritePairs]);
-   // Fetch position data periodically
-   useEffect(() => {
-      const fetchPositionData = async () => {
-         const positions = await api.getAllPositions();
-         setPositionData(positions);
-      };
-
-      fetchPositionData();
-      const intervalId = setInterval(fetchPositionData, 1000);
+    fetchPositionData();
+    const intervalId = setInterval(fetchPositionData, 1000);
 
     return () => clearInterval(intervalId);
   }, []);
@@ -90,122 +123,122 @@ const DashboardPage = () => {
     setCurrencyPairs(filteredPairs);
   }, [favoritePairs, allPairs, numDisplayedFavorites]);
 
-   const handleNumFavoritesChange = (newValue: string) => {
-      setNumDisplayedFavorites(parseInt(newValue, 10));
-   };
+  const handleNumFavoritesChange = (newValue: string) => {
+    setNumDisplayedFavorites(parseInt(newValue, 10));
+  };
 
-   const handleReplaceFavorite = (selectedPair: string, index: number) => {
+  const handleReplaceFavorite = (selectedPair: string, index: number) => {
+    setFavoritePairs(prev => {
+      const newFavorites = [...prev];
+      newFavorites[index] = selectedPair;
+      return newFavorites;
+    });
+  };
+  const getFavoritePairsOptions = () => {
+    const options = [];
+    for (let i = 1; i <= favoritePairs.length; i++) {
+      options.push(i);
+    }
+    return options;
+  };
+  const handleDragStart = (pair: string) => {
+    setDraggedItem(pair);
+  };
+
+  const handleDragOver = (event: React.DragEvent<HTMLElement>) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = (event: React.DragEvent<HTMLElement>, dropZoneId: string, index = -1) => {
+    event.preventDefault();
+    if (!draggedItem) return;
+
+    if (dropZoneId === 'favorites' && index !== -1) {
       setFavoritePairs(prev => {
-         const newFavorites = [...prev];
-         newFavorites[index] = selectedPair;
-         return newFavorites;
+        const newFavorites = [...prev];
+        const existingIndex = newFavorites.indexOf(draggedItem);
+
+        if (existingIndex !== -1) {
+          [newFavorites[index], newFavorites[existingIndex]] = [newFavorites[existingIndex], newFavorites[index]];
+        } else {
+          newFavorites[index] = draggedItem;
+        }
+
+        return newFavorites;
       });
-   };
-   const getFavoritePairsOptions = () => {
-      const options = [];
-      for (let i = 1; i <= favoritePairs.length; i++) {
-         options.push(i);
-      }
-      return options;
-   };
-   const handleDragStart = (pair: string) => {
-      setDraggedItem(pair);
-   };
+    }
 
-   const handleDragOver = (event: React.DragEvent<HTMLElement>) => {
-      event.preventDefault();
-   };
+    setDraggedItem(null);
+  };
 
-   const handleDrop = (event: React.DragEvent<HTMLElement>, dropZoneId: string, index = -1) => {
-      event.preventDefault();
-      if (!draggedItem) return;
+  return (
+    <OandaApiContext.Provider value={api}>
+      <div className="w-full flex pt-20 lg:pt-20 lg:p-6 p-4 flex-wrap ">
+        <div className="w-full flex flex-wrap gap-2 mb-4">
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button onClick={toggleProfile}>Account Summary</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <MasterProfile />
+            </DialogContent>
+          </Dialog>
 
-      if (dropZoneId === 'favorites' && index !== -1) {
-         setFavoritePairs(prev => {
-            const newFavorites = [...prev];
-            const existingIndex = newFavorites.indexOf(draggedItem);
+          <Select onValueChange={handleNumFavoritesChange} value={numDisplayedFavorites.toString()}>
+            <SelectTrigger>
+              <SelectValue>{numDisplayedFavorites} Favorites</SelectValue>{' '}
+            </SelectTrigger>
+            <SelectContent>
+              {getFavoritePairsOptions().map(n => (
+                <SelectItem key={n} value={n.toString()}>
+                  {n}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
 
-            if (existingIndex !== -1) {
-               [newFavorites[index], newFavorites[existingIndex]] = [newFavorites[existingIndex], newFavorites[index]];
-            } else {
-               newFavorites[index] = draggedItem;
-            }
-
-            return newFavorites;
-         });
-      }
-
-      setDraggedItem(null);
-   };
-
-   return (
-      <OandaApiContext.Provider value={api}>
-         <div className="w-full flex pt-20 lg:pt-20 lg:p-6 p-4 flex-wrap ">
-            <div className="w-full flex flex-wrap gap-2 mb-4">
-               <Dialog>
-                  <DialogTrigger asChild>
-                     <Button onClick={toggleProfile}>Account Summary</Button>
-                  </DialogTrigger>
-                  <DialogContent>
-                     <MasterProfile />
-                  </DialogContent>
-               </Dialog>
-
-               <Select onValueChange={handleNumFavoritesChange} value={numDisplayedFavorites.toString()}>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2 lg:gap-4 w-full">
+          {favoritePairs.slice(0, numDisplayedFavorites).map((pair, index) => (
+            <div key={pair} className="w-full p-3 lg:p-6 border border-gray-600 h-[420px] sm:h-[550px] md:h-[575px] lg:h-[650px] rounded-lg" onDrop={e => handleDrop(e, 'favorites', index)} onDragOver={handleDragOver} draggable onDragStart={() => handleDragStart(pair)}>
+              <a href={`/dashboard/pairs/${pair}`}>
+                <Stream pair={pair} data={streamData[pair]} />
+              </a>
+              <ResoModel pair={pair} streamData={streamData[pair]} selectedBoxArrayType={selectedBoxArrayTypes[pair]} />
+              <div className="w-full  flex justify-center items-center gap-2">
+                <Select value={selectedBoxArrayTypes[pair]} onValueChange={newValue => handleBoxArrayChange(pair, newValue)}>
                   <SelectTrigger>
-                     <SelectValue>{numDisplayedFavorites} Favorites</SelectValue>{' '}
+                    <SelectValue>{selectedBoxArrayTypes[pair]}</SelectValue>
                   </SelectTrigger>
                   <SelectContent>
-                     {getFavoritePairsOptions().map(n => (
-                        <SelectItem key={n} value={n.toString()}>
-                           {n}
-                        </SelectItem>
-                     ))}
+                    {Object.keys(BOX_SIZES).map(arrayKey => (
+                      <SelectItem key={arrayKey} value={arrayKey}>
+                        {arrayKey}
+                      </SelectItem>
+                    ))}
                   </SelectContent>
-               </Select>
+                </Select>
+                <Select value={pair} onValueChange={newValue => handleReplaceFavorite(newValue, index)}>
+                  <SelectTrigger>
+                    <SelectValue>{pair}</SelectValue>
+                  </SelectTrigger>
+                  <SelectContent>
+                    {currencyPairs.map(p => (
+                      <SelectItem key={p} value={p}>
+                        {p}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
+              <ElixrModel pair={pair} streamData={streamData[pair]} />
             </div>
-
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2 lg:gap-4 w-full">
-               {favoritePairs.slice(0, numDisplayedFavorites).map((pair, index) => (
-                  <div key={pair} className="w-full p-3 lg:p-6 border border-gray-600 h-[420px] sm:h-[550px] md:h-[575px] lg:h-[650px] rounded-lg" onDrop={e => handleDrop(e, 'favorites', index)} onDragOver={handleDragOver} draggable onDragStart={() => handleDragStart(pair)}>
-                     <a href={`/dashboard/pairs/${pair}`}>
-                        <Stream pair={pair} data={streamData[pair]} />
-                     </a>
-                     <ResoModel pair={pair} streamData={streamData[pair]} selectedBoxArrayType={selectedBoxArrayTypes[pair]} />
-                     <div className="w-full  flex justify-center items-center gap-2">
-                        <Select value={selectedBoxArrayTypes[pair]} onValueChange={newValue => handleBoxArrayChange(pair, newValue)}>
-                           <SelectTrigger>
-                              <SelectValue>{selectedBoxArrayTypes[pair]}</SelectValue>
-                           </SelectTrigger>
-                           <SelectContent>
-                              {Object.keys(BOX_SIZES).map(arrayKey => (
-                                 <SelectItem key={arrayKey} value={arrayKey}>
-                                    {arrayKey}
-                                 </SelectItem>
-                              ))}
-                           </SelectContent>
-                        </Select>
-                        <Select value={pair} onValueChange={newValue => handleReplaceFavorite(newValue, index)}>
-                           <SelectTrigger>
-                              <SelectValue>{pair}</SelectValue>
-                           </SelectTrigger>
-                           <SelectContent>
-                              {currencyPairs.map(p => (
-                                 <SelectItem key={p} value={p}>
-                                    {p}
-                                 </SelectItem>
-                              ))}
-                           </SelectContent>
-                        </Select>
-                     </div>
-                     <ElixrModel pair={pair} streamData={streamData[pair]} />
-                  </div>
-               ))}
-            </div>
-         </div>
-         <MasterPosition positionData={positionData} />
-      </OandaApiContext.Provider>
-   );
+          ))}
+        </div>
+      </div>
+      <MasterPosition positionData={positionData} />
+    </OandaApiContext.Provider>
+  );
 };
 
 export default DashboardPage;
