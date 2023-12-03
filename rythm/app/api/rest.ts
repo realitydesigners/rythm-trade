ef8a3f225135db7ec87c5e1ad47af29653690fd9
numbercutter <jamlets@protonmail.com>
2023-12-03 06:58:56 -0500

    comments

--- rythm/app/api/rest.ts
+++ rythm/app/api/rest.ts
@@ -1,6 +1,12 @@
 // src/api/rest.ts
 const serverBaseUrl = process.env.NEXT_PUBLIC_SERVER_URL;
 
+/**
+ * Fetches favorite currency pairs for a given user.
+ * @param {string} userId - The user's unique identifier.
+ * @returns {Promise<Array<string>>} A promise that resolves to an array of favorite currency pairs.
+ * @throws {Error} Throws an error if the network response is not ok.
+ */
 export const fetchFavoritePairs = async (userId: any) => {
   try {
     const response = await fetch(`${serverBaseUrl}/forex-preferences/${userId}`);
@@ -8,13 +14,19 @@ export const fetchFavoritePairs = async (userId: any) => {
       throw new Error('Network response was not ok');
     }
     const data = await response.json();
-    return data.pairs.map((pair: { pair: any; }) => pair.pair);
+    return data.pairs.map((pair: { pair: any }) => pair.pair);
   } catch (error) {
     console.error('Error fetching favorite pairs:', error);
     throw error;
   }
 };
 
+/**
+ * Updates the favorite currency pairs for a given user.
+ * @param {string} userId - The user's unique identifier.
+ * @param {Array<string>} newPairs - An array of new favorite currency pairs.
+ * @throws {Error} Throws an error if the network response is not ok.
+ */
 export const updateFavoritePairs = async (userId: any, newPairs: any) => {
   try {
     const response = await fetch(`${serverBaseUrl}/forex-preferences`, {
@@ -33,6 +45,12 @@ export const updateFavoritePairs = async (userId: any, newPairs: any) => {
   }
 };
 
+/**
+ * Fetches available trading instruments for a given user.
+ * @param {string} userId - The user's unique identifier.
+ * @returns {Promise<Array<string>>} A promise that resolves to an array of instrument names.
+ * @throws {Error} Throws an error if the network response is not ok.
+ */
 export const fetchInstruments = async (userId: any) => {
   try {
     const response = await fetch(`${serverBaseUrl}/instruments/${userId}`);
@@ -40,24 +58,29 @@ export const fetchInstruments = async (userId: any) => {
       throw new Error('Failed to fetch instruments');
     }
     const instruments = await response.json();
-    return instruments.map((inst: { name: any; }) => inst.name);
+    return instruments.map((inst: { name: any }) => inst.name);
   } catch (error) {
     console.error('Error fetching instruments:', error);
     throw error;
   }
 };
 
+/**
+ * Fetches all trading positions for a given user.
+ * @param {string} userId - The user's unique identifier.
+ * @returns {Promise<any>} A promise that resolves to the positions data.
+ * @throws {Error} Throws an error if the network response is not ok.
+ */
 export const fetchAllPositions = async (userId: any) => {
-    try {
-      const response = await fetch(`${serverBaseUrl}/positions/${userId}`);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const positions = await response.json();
-      return positions;
-    } catch (error) {
-      console.error('Error fetching positions:', error);
-      throw error;
+  try {
+    const response = await fetch(`${serverBaseUrl}/positions/${userId}`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
-  };
-  
+    const positions = await response.json();
+    return positions;
+  } catch (error) {
+    console.error('Error fetching positions:', error);
+    throw error;
+  }
+};
