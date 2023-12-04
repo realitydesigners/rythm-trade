be336cb05bf74d0a48497718ead937da81519a48
RD <raymond.luke.spartz@gmail.com>
2023-12-04 09:12:25 -0800

    ya

--- rythm/app/(user)/account/AccountForm.tsx
+++ rythm/app/(user)/account/AccountForm.tsx
@@ -3,11 +3,12 @@ import React, { useState } from 'react';
 
 export default function AccountForm() {
    const [accountId, setAccountId] = useState('');
+   const [apiKey, setApiKey] = useState('');
 
    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
       event.preventDefault();
-      // Process accountId, e.g., send to your API or backend
-      console.log(accountId); // Placeholder for actual submission logic
+      // Process accountId and apiKey, e.g., send to your API or backend
+      console.log(accountId, apiKey); // Placeholder for actual submission logic
    };
 
    return (
@@ -18,6 +19,12 @@ export default function AccountForm() {
                   Account ID
                </label>
                <input id="accountId" name="accountId" type="text" value={accountId} onChange={e => setAccountId(e.target.value)} placeholder="Enter your Account ID" className=" font-mono shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+            </div>
+            <div className="mb-4">
+               <label htmlFor="apiKey" className="block font-mono text-gray-600 text-sm font-bold mb-2">
+                  API Key
+               </label>
+               <input id="apiKey" name="apiKey" type="text" value={apiKey} onChange={e => setApiKey(e.target.value)} placeholder="Enter your API Key" className="font-mono shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
             </div>
             <button type="submit" className="w-full font-mono bg-white hover:bg-gray-200/50 text-black font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105">
                Save
