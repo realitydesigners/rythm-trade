63c7082fb50bf7bd4e1cc1613a2413cdccb7bca3
RD <raymond.luke.spartz@gmail.com>
2023-12-05 11:42:55 -0800

    add img to next config

--- rythm/app/(user)/account/page.tsx
+++ rythm/app/(user)/account/page.tsx
@@ -16,7 +16,7 @@ export default async function AccountPage() {
       <div className="flex justify-center items-center h-screen  p-4">
          <div className="max-w-2xl w-full rounded-lg shadow-xl border border-gray-600 overflow-hidden">
             <div className="p-8 bg-black text-white text-center">
-               {user?.imageUrl && <Image src={user.imageUrl} alt="Profile" className="w-20 h-20 border rounded-full object-cover mx-auto mb-6" />}
+               {user?.imageUrl && <Image src={user.imageUrl} alt="Profile" width="100" height="100" className=" border rounded-full object-cover mx-auto mb-6" />}
                <h1 className="text-4xl font-bold mb-4">Welcome, {user?.firstName}!</h1>
             </div>
 
