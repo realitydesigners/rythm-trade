54bd0c604291b00d23948d4fa7c74790a8170a48
RD <raymond.luke.spartz@gmail.com>
2023-12-14 12:45:36 -0800

    add route

--- rythm/middleware.ts
+++ rythm/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-   publicRoutes: ['/', '/about', '/contact'],
+   publicRoutes: ['/', '/about', '/contact', '/api/send'],
 });
 
 export const config = {
