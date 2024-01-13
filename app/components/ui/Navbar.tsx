4a5928a72c0db763ac78dbe0df63400d4503c387
RD <raymond.luke.spartz@gmail.com>
2024-01-13 14:31:59 -0800

    rm about page

--- app/components/ui/Navbar.tsx
+++ app/components/ui/Navbar.tsx
@@ -164,18 +164,7 @@ export default function Navbar() {
 						>
 							{/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
 							<svg className="w-4 h-4">{getIcon("dashboard")}</svg>
-							<span className="ml-2">Tools</span>
-						</Link>
-					</li>
-					<li className="block">
-						<Link
-							href="/about"
-							className={`${monomaniac.className}  flex items-center  text-3xl lg:text-sm text-gray-200  tracking-wide  font-semibold hover:bg-gray-600/30 hover:text-gray-200 hover:scale-105 p-2 rounded-lg transition-all duration-200 ease-in-out`}
-							onClick={closeNav}
-						>
-							{/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
-							<svg className="w-4 h-4">{getIcon("dashboard")}</svg>
-							<span className="ml-2">About</span>
+							<span className="ml-2">Our Tools</span>
 						</Link>
 					</li>
 				</ul>
