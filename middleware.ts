837f5b804a279d9ba9a54986854d825f6d41de12
RD <raymond.luke.spartz@gmail.com>
2024-02-21 09:18:42 -0800

    formatting and move shadcn utils to same folder as project

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
-import { authMiddleware } from '@clerk/nextjs';
+import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-   publicRoutes: ['/', '/about', '/contact'],
+	publicRoutes: ["/", "/about", "/contact"],
 });
 
 export const config = {
-   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/(api|trpc)(.*)'],
+	matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)"],
 };
