49712da0bec377b7c8ffa8c6c1d2841bee3c99d0
RD <raymond.luke.spartz@gmail.com>
2024-03-10 14:00:10 -0700

    toast notif and fix form

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,12 +1,18 @@
 import { WebSocketProvider } from "@/app/components/context/WebSocketContext";
 import Navbar from "@/app/components/ui/Navbar";
+import { Toaster } from "@/components/ui/sonner";
 
 import { Metadata } from "next";
 
 export default function RootLayout({
-	children,
+    children,
 }: {
-	children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-	return <WebSocketProvider>{children}</WebSocketProvider>;
+    return (
+        <WebSocketProvider>
+            {children}
+            <Toaster />
+        </WebSocketProvider>
+    );
 }
